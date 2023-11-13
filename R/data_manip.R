8262be4316af34e6fa2c5fd32dfc47077ac835b5
Karim-Mane <karimanee@outlook.com>
2023-11-13 09:01:56 +0000

    update vignette and filter_snps_sample() function

--- R/data_manip.R
+++ R/data_manip.R
@@ -397,12 +397,13 @@ print.SNPdata <- function(snpdata) {
 #'  }
 #' @export
 #'
-filter_snps_samples <- function(snpdata, min_qual   = 10L,
+filter_snps_samples <- function(snpdata,
+                                min_qual            = 10L,
                                 max_missing_sites   = 0.2,
                                 max_missing_samples = 0.2,
                                 maf_cutoff          = 0.01) {
   checkmate::assert_class(snpdata, "SNPdata", null.ok = FALSE)
-  checkmate::assert_integer(min_qual, lower = 10L, any.missing = FALSE,
+  checkmate::assert_numeric(min_qual, lower = 10L, any.missing = FALSE,
                             null.ok = FALSE)
   checkmate::assert_numeric(max_missing_sites, lower = 0L, upper = 1L,
                             finite = TRUE, any.missing = FALSE, null.ok = FALSE,
@@ -415,60 +416,54 @@ filter_snps_samples <- function(snpdata, min_qual   = 10L,
                             len = 1L)
   x      <- snpdata[["details"]]
   fields <- c("GT", "Phased", "Phased_Imputed")
-  if (all(missing(min_qual) &&
-            missing(max_missing_sites) &&
-            missing(max_missing_samples))) {
-    return(snpdata)
-  } else {
-    idx <- which(x[["Qual"]] >= min_qual &
-                   x[["percentage_missing_samples"]] <= max_missing_samples &
-                   x[["MAF"]] >= maf_cutoff)
-    if (all(length(idx) > 0L && length(idx) < nrow(snpdata[["details"]]))) {
-      x <- x[idx, ]
-      snpdata[["details"]] <- x
-      for (field in fields) {
-        if (field %in% names(snpdata)) {
-          snpdata[[field]] <- snpdata[[field]][idx, ]
-        }
+  idx    <- which(x[["Qual"]] >= min_qual &
+                    x[["percentage_missing_samples"]] <= max_missing_samples &
+                    x[["MAF"]] >= maf_cutoff)
+  stopifnot("\nNo locus in VCF file has satisfied the specified QC metrics." =
+              length(idx) != 0L)
+  if (all(length(idx) > 0L && length(idx) < nrow(snpdata[["details"]]))) {
+    x    <- x[idx, ]
+    snpdata[["details"]] <- x
+    for (field in fields) {
+      if (field %in% names(snpdata)) {
+        snpdata[[field]] <- snpdata[[field]][idx, ]
       }
-      Chrom <- Pos <- NULL # nolint
-      f2c        <- x %>% dplyr::select(Chrom, Pos)
-      output_dir <- dirname(snpdata[["vcf"]])
-      data.table::fwrite(f2c, file.path(output_dir, "loci_to_be_retained.txt"),
-                         col.names = FALSE,
-                         row.names = FALSE,
-                         quote     = FALSE,
-                         sep       = "\t",
-                         nThread   = 4L)
-      snpdata[["vcf"]] <- remove_snps_from_vcf(snpdata[["vcf"]],
-                                               "loci_to_be_retained.txt",
-                                               output_dir,
-                                               index = snpdata[["index"]])
-    } else if (length(idx) == 0L) {
-      stop("\nNo locus in VCF file has satisfied specified the QC metrics")
-    } else if (length(idx) == nrow(snpdata[["details"]])) {
-      message("all loci have satisfied the specified QC metrics")
     }
+    Chrom <- Pos <- NULL
+    f2c          <- x %>% dplyr::select(Chrom, Pos)
+    output_dir   <- dirname(snpdata[["vcf"]])
+    data.table::fwrite(f2c, file.path(output_dir, "loci_to_be_retained.txt"),
+                       col.names = FALSE,
+                       row.names = FALSE,
+                       quote     = FALSE,
+                       sep       = "\t",
+                       nThread   = 4L)
+    snpdata[["vcf"]] <- remove_snps_from_vcf(snpdata[["vcf"]],
+                                             "loci_to_be_retained.txt",
+                                             output_dir,
+                                             index = snpdata[["index"]])
+  } else if (length(idx) == nrow(snpdata[["details"]])) {
+    message("All loci have satisfied the specified QC metrics.")
+  }
 
-    idx <- which(snpdata[["meta"]][["percentage_missing_sites"]] <=
-                   max_missing_sites)
-    if (all(length(idx) > 0L & length(idx) < nrow(snpdata[["meta"]]))) {
-      message("\nthe following samples will be removed:\n",
-              paste(snpdata[["meta"]][["sample"]], collapse = "\n"))
-      snpdata[["meta"]] <- snpdata[["meta"]][idx, ]
-      data.table::fwrite(snpdata[["meta"]][["sample"]],
-                         file.path(output_dir, "samples_to_be_dropped.txt"),
-                         col.names = FALSE, row.names = FALSE,
-                         quote = FALSE, sep = "\t", nThread = 4L)
-      snpdata[["vcf"]] <- remove_samples_from_vcf(snpdata[["vcf"]],
-                                                  "samples_to_be_dropped.txt",
-                                                  output_dir,
-                                                  index = snpdata[["index"]])
-    } else if (length(idx) == 0L) {
-      stop("\nNo sample in VCF file has satisfied the specified QC metrics.")
-    } else if (length(idx) == nrow(snpdata[["meta"]])) {
-      message("\nAll samples have satisfied the specified QC metrics.")
-    }
+  idx <- which(snpdata[["meta"]][["percentage_missing_sites"]] <=
+                 max_missing_sites)
+  stopifnot("\nNo sample in VCF file has satisfied the specified QC metrics." =
+              length(idx) != 0L)
+  if (all(length(idx) > 0L && length(idx) < nrow(snpdata[["meta"]]))) {
+    message("\nthe following samples will be removed:\n",
+            paste(snpdata[["meta"]][["sample"]][-idx], collapse = "\n"))
+    snpdata[["meta"]] <- snpdata[["meta"]][idx, ]
+    data.table::fwrite(snpdata[["meta"]][["sample"]][-idx],
+                       file.path(output_dir, "samples_to_be_dropped.txt"),
+                       col.names = FALSE, row.names = FALSE,
+                       quote = FALSE, sep = "\t", nThread = 4L)
+    snpdata[["vcf"]] <- remove_samples_from_vcf(snpdata[["vcf"]],
+                                                "samples_to_be_dropped.txt",
+                                                output_dir,
+                                                index = snpdata[["index"]])
+  } else if (length(idx) == nrow(snpdata[["meta"]])) {
+    message("\nAll samples have satisfied the specified QC metrics.")
   }
 
   snpdata[["index"]] <- snpdata[["index"]] + 1L
