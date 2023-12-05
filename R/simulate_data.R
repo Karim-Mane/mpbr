4eacb98de575dd68b12d499c301df8eb34203d6f
Karim-Mane <karimanee@outlook.com>
2023-12-05 08:21:19 +0000

    apply best practices on simulate_data() function

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,65 +1,76 @@
-#################################################################################
-# function to simulate data given parameters, given fs and gendist
-# fs should be a matrix with ndata rows
-# and a number of columns equal to the max number of possible allele types
-# if a certain position has a smaller number of possible allele types 
-# then the corresponding row of fs should contain strictly positive values and then zeros.
-#
-# Example: if 3 types are possible at position p, but 5 types are possible
-# somewhere else, then fs should have five columns, and fs[p,] might look like
-# [0.2, 0.3, 0.5, 0, 0]
-
-# gendist should be a vector where gendist[p] contains the distance between position p and p+1
-# or equivalently, gendist[p-1] contains the distance between position p-1 and p, for p > 1.
-# so only ndata-1 first entries of gendist are being used.
-#################################################################################
+#' Simulate data given parameters, the genotype matrix fs and the genetic
+#' distance between loci.
+#'
+#' @param fs a matrix of genotype data with N rows and M columns where N is the
+#'      number of loci and M the number of possible allele types.
+#'      Example: if 3 types are possible at position p, but 5 types are possible
+#'      somewhere else, then fs should have five columns, and fs[p,] might look
+#'      like [0.2, 0.3, 0.5, 0, 0]
+#' @param gendist a vector of genetic distance between loci where gendist[p]
+#'      contains the distance between position p and p+1 or equivalently,
+#'      gendist[p-1] contains the distance between position p-1 and p, for p > 1
+#' @param k the k
+#' @param r the r
+#' @param epsilon the epsilon
+#' @param rho the rho
+#'
+#' @return a matrix of 2 columns and N rows where N is the number of loci in the
+#'      genotype matrix.
+#' @keywords internal
+#'
 simulate_data <- function(fs, gendist, k, r,
                           epsilon = 0.001,
-                          rho     = 7.4 * 10^(-7)){
-    ndata      <- dim(fs)[[1L]]
-    maxnstates <- dim(fs)[[2L]]
-    nstates    <- 0L
-    Ys         <- matrix(NA, nrow = ndata, ncol = 2L)
-    for (position in seq_len(ndata)) {
-        if (position == 1L){
-            IBD_current <- (runif(1L) <= r) # Bernoulli(r)
-        } else {
-            if (IBD_current) {
-                IBD_current <- (runif(1L) < (1L - (1L - r) * (1 - exp(-k * rho * gendist[position - 1L])))) # nolint: line_length_linter
-            } else {
-                IBD_current <- (runif(1L) < r*(1L - exp(-k * rho * gendist[position - 1L]))) # nolint: line_length_linter
-            }
-        }
-        # number of possible allele types at current position
-        nstates <- 1
-        while((nstates < maxnstates) && (fs[position,nstates] > 1e-10)){
-            nstates <- nstates + 1
-        }
-        #
-        Gi <- sample(x = 0:(nstates-1), size = 1, prob = fs[position,1:nstates])
-        Gj <- NA; Yi <- NA; Yj <- NA
-        # generate Gi, Gj given IBD_current
-        if (IBD_current){
-            Gj <- Gi
-        } else {
-            Gj <- sample(x = 0:(nstates-1), size = 1, prob = fs[position,1:nstates])
-        }
-        # generate Yi, Yj given Gi, Gj,
-        # i.e. genotyping error part of the model
-        if (runif(1) < (1 - (nstates-1)*epsilon)){
-            Yi <- Gi # no error
-        } else {
-            # sample uniformly on the other possible states
-            otherstates <- setdiff(0:(nstates-1), Gi)
-            Yi <- sample(x = otherstates, size = 1)
-        }
-        if (runif(1) < (1 - (nstates-1)*epsilon)){
-            Yj <- Gj
-        } else {
-            otherstates <- setdiff(0:(nstates-1), Gj)
-            Yj <- sample(x = otherstates, size = 1)
-        }
-        Ys[position,] <- c(Yi,Yj) 
+                          rho     = 7.4 * 10^(-7)) { # nolint: implicit_integer_linter
+  ndata      <- dim(fs)[[1L]]
+  maxnstates <- dim(fs)[[2L]]
+  nstates    <- 0L
+  Ys         <- matrix(NA, nrow = ndata, ncol = 2L) # nolint: object_name_linter
+  for (position in seq_len(ndata)) {
+    if (position == 1L) {
+      IBD_current <- (runif(1L) <= r) # nolint: object_name_linter
+    } else {
+      if (IBD_current) {
+        IBD_current <- (runif(1L) < (1L - (1L - r) * (1 - exp(-k * rho * gendist[position - 1L])))) # nolint: line_length_linter
+      } else {
+        IBD_current <- (runif(1L) < r*(1L - exp(-k * rho * gendist[position - 1L]))) # nolint: line_length_linter
+      }
     }
-    return(Ys)
+    # number of possible allele types at current position
+    nstates <- 1L
+    while ((nstates < maxnstates) && (fs[position, nstates] > 1e-10)) {
+      nstates <- nstates + 1L
+    }
+    #
+    Gi <- sample(x    = 0L:(nstates - 1L), # nolint: object_name_linter
+                 size = 1L,
+                 prob = fs[position, 1L:nstates])
+    Gj <- NA # nolint: object_name_linter
+    Yi <- NA # nolint: object_name_linter
+    Yj <- NA # nolint: object_name_linter
+    # generate Gi, Gj given IBD_current
+    if (IBD_current) {
+      Gj <- Gi # nolint: object_name_linter
+    } else {
+      Gj <- sample(x    = 0L:(nstates - 1L), # nolint: object_name_linter
+                   size = 1L,
+                   prob = fs[position, 1L:nstates])
+    }
+    # generate Yi, Yj given Gi, Gj,
+    # i.e. genotyping error part of the model
+    if (runif(1L) < (1L - (nstates - 1L) * epsilon)) {
+      Yi <- Gi # nolint: object_name_linter
+    } else {
+      # sample uniformly on the other possible states
+      otherstates <- setdiff(0L:(nstates - 1L), Gi)
+      Yi <- sample(x = otherstates, size = 1L) # nolint: object_name_linter
+    }
+    if (runif(1L) < (1L - (nstates - 1L) * epsilon)) {
+      Yj <- Gj # nolint: object_name_linter
+    } else {
+      otherstates <- setdiff(0L:(nstates - 1L), Gj)
+      Yj <- sample(x = otherstates, size = 1L) # nolint: object_name_linter
+    }
+    Ys[position, ] <- c(Yi, Yj) # nolint: object_name_linter
+  }
+  return(Ys)
 }
