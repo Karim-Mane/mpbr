3dc27f77ba17aedf903a0c4b32f968b4c6d83f5b
Karim-Mane <84502011+Karim-Mane@users.noreply.github.com>
2024-05-28 22:39:52 +0000

    Deploying to gh-pages from @ Karim-Mane/mpbr@79a6625a592d859617922c918aee37eaef742ca3 🚀

--- pkgdown.js
+++ pkgdown.js
@@ -2,43 +2,83 @@
 (function($) {
   $(function() {
 
-    $('nav.navbar').headroom();
+    $('.navbar-fixed-top').headroom();
 
-    Toc.init({
-      $nav: $("#toc"),
-      $scope: $("main h2, main h3, main h4, main h5, main h6")
+    $('body').css('padding-top', $('.navbar').height() + 10);
+    $(window).resize(function(){
+      $('body').css('padding-top', $('.navbar').height() + 10);
     });
 
-    if ($('#toc').length) {
-      $('body').scrollspy({
-        target: '#toc',
-        offset: $("nav.navbar").outerHeight() + 1
-      });
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var cur_path = paths(location.pathname);
+    var links = $("#navbar ul li a");
+    var max_length = -1;
+    var pos = -1;
+    for (var i = 0; i < links.length; i++) {
+      if (links[i].getAttribute("href") === "#")
+        continue;
+      // Ignore external links
+      if (links[i].host !== location.host)
+        continue;
+
+      var nav_path = paths(links[i].pathname);
+
+      var length = prefix_length(nav_path, cur_path);
+      if (length > max_length) {
+        max_length = length;
+        pos = i;
+      }
     }
 
-    // Activate popovers
-    $('[data-bs-toggle="popover"]').popover({
-      container: 'body',
-      html: true,
-      trigger: 'focus',
-      placement: "top",
-      sanitize: false,
-    });
+    // Add class to parent <li>, and enclosing <li> if in dropdown
+    if (pos >= 0) {
+      var menu_anchor = $(links[pos]);
+      menu_anchor.parent().addClass("active");
+      menu_anchor.closest("li.dropdown").addClass("active");
+    }
+  });
 
-    $('[data-bs-toggle="tooltip"]').tooltip();
+  function paths(pathname) {
+    var pieces = pathname.split("/");
+    pieces.shift(); // always starts with /
+
+    var end = pieces[pieces.length - 1];
+    if (end === "index.html" || end === "")
+      pieces.pop();
+    return(pieces);
+  }
+
+  // Returns -1 if not found
+  function prefix_length(needle, haystack) {
+    if (needle.length > haystack.length)
+      return(-1);
+
+    // Special case for length-0 haystack, since for loop won't run
+    if (haystack.length === 0) {
+      return(needle.length === 0 ? 0 : -1);
+    }
+
+    for (var i = 0; i < haystack.length; i++) {
+      if (needle[i] != haystack[i])
+        return(i);
+    }
+
+    return(haystack.length);
+  }
 
   /* Clipboard --------------------------*/
 
   function changeTooltipMessage(element, msg) {
-    var tooltipOriginalTitle=element.getAttribute('data-bs-original-title');
-    element.setAttribute('data-bs-original-title', msg);
+    var tooltipOriginalTitle=element.getAttribute('data-original-title');
+    element.setAttribute('data-original-title', msg);
     $(element).tooltip('show');
-    element.setAttribute('data-bs-original-title', tooltipOriginalTitle);
+    element.setAttribute('data-original-title', tooltipOriginalTitle);
   }
 
   if(ClipboardJS.isSupported()) {
     $(document).ready(function() {
-      var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='tooltip' data-placement='left' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy'></i></button>";
+      var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy'></i></button>";
 
       $("div.sourceCode").addClass("hasCopyButton");
 
@@ -49,191 +89,20 @@
       $('.btn-copy-ex').tooltip({container: 'body'});
 
       // Initialize clipboard:
-      var clipboard = new ClipboardJS('[data-clipboard-copy]', {
+      var clipboardBtnCopies = new ClipboardJS('[data-clipboard-copy]', {
         text: function(trigger) {
           return trigger.parentNode.textContent.replace(/\n#>[^\n]*/g, "");
         }
       });
 
-      clipboard.on('success', function(e) {
+      clipboardBtnCopies.on('success', function(e) {
         changeTooltipMessage(e.trigger, 'Copied!');
         e.clearSelection();
       });
 
-      clipboard.on('error', function(e) {
+      clipboardBtnCopies.on('error', function() {
         changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
       });
-
     });
   }
-
-    /* Search marking --------------------------*/
-    var url = new URL(window.location.href);
-    var toMark = url.searchParams.get("q");
-    var mark = new Mark("main#main");
-    if (toMark) {
-      mark.mark(toMark, {
-        accuracy: {
-          value: "complementary",
-          limiters: [",", ".", ":", "/"],
-        }
-      });
-    }
-
-  /* Search --------------------------*/
-  /* Adapted from https://github.com/rstudio/bookdown/blob/2d692ba4b61f1e466c92e78fd712b0ab08c11d31/inst/resources/bs4_book/bs4_book.js#L25 */
-    // Initialise search index on focus
-  var fuse;
-  $("#search-input").focus(async function(e) {
-    if (fuse) {
-      return;
-    }
-
-    $(e.target).addClass("loading");
-    var response = await fetch($("#search-input").data("search-index"));
-    var data = await response.json();
-
-    var options = {
-      keys: ["what", "text", "code"],
-      ignoreLocation: true,
-      threshold: 0.1,
-      includeMatches: true,
-      includeScore: true,
-    };
-    fuse = new Fuse(data, options);
-
-    $(e.target).removeClass("loading");
-  });
-
-  // Use algolia autocomplete
-  var options = {
-    autoselect: true,
-    debug: true,
-    hint: false,
-    minLength: 2,
-  };
-  var q;
-async function searchFuse(query, callback) {
-  await fuse;
-
-  var items;
-  if (!fuse) {
-    items = [];
-  } else {
-    q = query;
-    var results = fuse.search(query, { limit: 20 });
-    items = results
-      .filter((x) => x.score <= 0.75)
-      .map((x) => x.item);
-    if (items.length === 0) {
-      items = [{dir:"Sorry 😿",previous_headings:"",title:"No results found.",what:"No results found.",path:window.location.href}];
-    }
-  }
-  callback(items);
-}
-  $("#search-input").autocomplete(options, [
-    {
-      name: "content",
-      source: searchFuse,
-      templates: {
-        suggestion: (s) => {
-          if (s.title == s.what) {
-            return `${s.dir} >	<div class="search-details"> ${s.title}</div>`;
-          } else if (s.previous_headings == "") {
-            return `${s.dir} >	<div class="search-details"> ${s.title}</div> > ${s.what}`;
-          } else {
-            return `${s.dir} >	<div class="search-details"> ${s.title}</div> > ${s.previous_headings} > ${s.what}`;
-          }
-        },
-      },
-    },
-  ]).on('autocomplete:selected', function(event, s) {
-    window.location.href = s.path + "?q=" + q + "#" + s.id;
-  });
-  });
 })(window.jQuery || window.$)
-
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2023 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- * Updates for {pkgdown} by the {bslib} authors, also licensed under CC-BY-3.0.
- */
-
-const getStoredTheme = () => localStorage.getItem('theme')
-const setStoredTheme = theme => localStorage.setItem('theme', theme)
-
-const getPreferredTheme = () => {
-  const storedTheme = getStoredTheme()
-  if (storedTheme) {
-    return storedTheme
-  }
-
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-}
-
-const setTheme = theme => {
-  if (theme === 'auto') {
-    document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
-  } else {
-    document.documentElement.setAttribute('data-bs-theme', theme)
-  }
-}
-
-function bsSetupThemeToggle () {
-  'use strict'
-
-  const showActiveTheme = (theme, focus = false) => {
-    var activeLabel, activeIcon;
-
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      const buttonTheme = element.getAttribute('data-bs-theme-value')
-      const isActive = buttonTheme == theme
-      
-      element.classList.toggle('active', isActive)
-      element.setAttribute('aria-pressed', isActive)
-
-      if (isActive) {
-        activeLabel = element.textContent;
-        activeIcon = element.querySelector('span').classList.value;
-      }
-    })
-
-    const themeSwitcher = document.querySelector('#dropdown-lightswitch')
-    if (!themeSwitcher) {
-      return
-    }
-
-    themeSwitcher.setAttribute('aria-label', activeLabel)
-    themeSwitcher.querySelector('span').classList.value = activeIcon;
-
-    if (focus) {
-      themeSwitcher.focus()
-    }
-  }
-
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    const storedTheme = getStoredTheme()
-    if (storedTheme !== 'light' && storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
-    }
-  })
-
-  window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
-
-    document
-      .querySelectorAll('[data-bs-theme-value]')
-      .forEach(toggle => {
-        toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-bs-theme-value')
-          setTheme(theme)
-          setStoredTheme(theme)
-          showActiveTheme(theme, true)
-        })
-      })
-  })
-}
-
-setTheme(getPreferredTheme());
-bsSetupThemeToggle();
