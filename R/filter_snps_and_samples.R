35377dd8b2d07bebe38eef31efd34c2bf548fe4b
Maranita <93143789+Maranita@users.noreply.github.com>
2024-08-13 15:02:57 +0000

    A new parameter has been added to the function, allowing users to choose between two methods for filtering missing data. Users can either directly filter based on the specified threshold or follow a step-by-step iterative approach. If the provided threshold is less than 80 percent, the function will first filter out samples and loci with 80 percent missingness. Next, it will compute the missingness for the resulting dataset. If the threshold is then found to be less than 50 percent, the function will filter out samples and loci with 50 percent missingness before applying the specified threshold. This enhancement offers users greater flexibility and precision in managing missing data effectively.

--- R/filter_snps_and_samples.R
+++ R/filter_snps_and_samples.R
@@ -9,6 +9,8 @@
 #'    a sample should be discarded. default = 0.2
 #' @param max_missing_samples the maximum fraction of missing samples above
 #'    which a loci should be discarded. default = 0.2
+#'  @param iterative_fileration enables recursive filtration of missingness
+#'   for loci and samples. default = TRUE 
 #' @param maf_cutoff the MAF cut-off. loci with a MAF < maf_cutoff will be
 #'    discarded
 #'
@@ -20,6 +22,7 @@
 #'   min_qual            = 10,
 #'   max_missing_sites   = 0.2,
 #'   max_missing_samples = 0.2,
+#'   iterative_fileration = TRUE,
 #'   maf_cutoff          = 0.01
 #'  )
 #'  }
@@ -29,6 +32,7 @@ filter_snps_samples <- function(snpdata,
                                 min_qual            = 10L,
                                 max_missing_sites   = 0.2,
                                 max_missing_samples = 0.2,
+                                iterative_fileration = TRUE,
                                 maf_cutoff          = 0.01) {
   checkmate::assert_class(snpdata, "SNPdata", null.ok = FALSE)
   checkmate::assert_numeric(min_qual, lower = 10L, any.missing = FALSE,
@@ -39,10 +43,46 @@ filter_snps_samples <- function(snpdata,
   checkmate::assert_numeric(max_missing_samples, lower = 0L, upper = 1L,
                             finite = TRUE, any.missing = FALSE, null.ok = FALSE,
                             len = 1L)
+  checkmate::assert_logical(iterative_fileration, any.missing = FALSE,
+                            null.ok = FALSE)
   checkmate::assert_numeric(maf_cutoff, lower = 0L, upper = 1L,
                             finite = TRUE, any.missing = FALSE, null.ok = FALSE,
                             len = 1L)
-  x      <- snpdata[["details"]]
+  
+  if(iterative_fileration == TRUE){
+    x <- snpdata[["details"]]
+    y <- snpdata[["meta"]]
+    snps <- snpdata[["GT"]]
+    if (max_missing_samples <= 80)
+    {
+      snps <- snps[which(x[["percentage_missing_samples"]] <= 0.8), which(y[["percentage_missing_sites"]] <= 0.8)]
+      x= x[x[["percentage_missing_samples"]] <= 0.8,]
+      y= y[y[["percentage_missing_sites"]] <= 0.8,]
+      #compute missingness
+      y[["percentage_missing_sites"]]      <- colSums(is.na(snps)) / nrow(snps)
+      x[["percentage_missing_samples"]] <- rowSums(is.na(snps)) / ncol(snps)
+    }
+    
+    if (max_missing_samples <= 50)
+    {
+      #filter 50% missingness for sample and loci
+      snps <- snp[which(x[["percentage_missing_samples"]] <= 0.5), which(y[["percentage_missing_sites"]] <= 0.5)]
+      x= x[x[["percentage_missing_samples"]] <= 0.5,]
+      y= y[y[["percentage_missing_sites"]] <= 0.5,]
+      #compute missingness
+      y[["percentage_missing_sites"]]      <- colSums(is.na(snps)) / nrow(snps)
+      x[["percentage_missing_samples"]] <- rowSums(is.na(snps)) / ncol(snps)
+      snpdata[["details"]] <- x
+      snpdata[["meta"]] <- x
+      snpdata[["GT"]] <- snps
+      
+    }
+  }
+  else{
+    x      <- snpdata[["details"]]
+    
+  }
+  
   fields <- c("GT", "Phased", "Phased_Imputed")
   idx    <- which(x[["Qual"]] >= min_qual &
                     x[["percentage_missing_samples"]] <= max_missing_samples &
