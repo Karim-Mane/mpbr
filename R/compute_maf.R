fc6046f729027027588c414938f2f9485922541c
BrandonNgwa <ngwabrandon10@gmail.com>
2024-09-23 00:52:34 -1200

    Ched stopifnot to stop

--- R/compute_maf.R
+++ R/compute_maf.R
@@ -47,11 +47,13 @@ compute_maf <- function(snpdata, include_het = FALSE, mat_name = "GT",
     mat_name,
     choices = c("GT", "Phased", "Imputed", "Phased_imputed")
   )
+  
   if (include_het) {
-    stopifnot("'include_het = TRUE' is only valid for the raw genotype data in
-              the 'GT' matrix or the imputed (non-phased) data in the 'Imputed'
-              matrix" = any(mat_name %in% c("GT", "Imputed")))
+    if (!any(mat_name %in% c("GT", "Imputed"))) {
+      stop("'include_het = TRUE' is only valid for 'GT' or 'Imputed' matrices")
+    }
   }
+  
   x   <- snpdata[[mat_name]]
   ref <- rowSums(x == 0L, na.rm = TRUE)
   alt <- rowSums(x == 1L, na.rm = TRUE)
