76ad2ff453a3dcd8e6fa305c07bf0749c322eefc
BrandonNgwa <ngwabrandon10@gmail.com>
2024-09-23 01:31:37 -1200

    Revert "added a condition to use gzip for Windows and continue with pigz and gzcat for other systems"
    This reverts commit d715cbda49f5a782fdb97129b9f401525fda3120.

--- R/access_genotype_data.R
+++ R/access_genotype_data.R
@@ -8,37 +8,23 @@
 #' @importFrom data.table %like%
 #' @author Banky
 extract_genotype <- function(file) {
-  ## check your operating system type
-  os_type <- Sys.info()["sysname"]
+  vcf <- data.table::fread(
+    cmd     = sprintf("pigz -dc < %s", file),
+    nThread = (parallel::detectCores() - 2),
+    sep     = NULL, # line is column
+    header  = FALSE # 1st line is data
+  )
   
-  if (os_type == "Windows") {
-    vcf <- data.table::fread(
-      cmd     = sprintf("gzip -dc < %s", file),
-      nThread = (parallel::detectCores() - 2),
-      sep     = NULL, # line is column
-      header  = FALSE # 1st line is data
-    )
-  } else {
-    vcf <- data.table::fread(
-      cmd     = sprintf("pigz -dc < %s", file),
-      nThread = (parallel::detectCores() - 2),
-      sep     = NULL, # line is column
-      header  = FALSE # 1st line is data
-    )
-  }
+  # DT name for unnamed col
+  V1 <- NULL # nolint: object_name_linter.
+  row_id <- vcf[V1 %like% "^#CHROM", which = TRUE] + 1 # + 1 skip header
   
-  # find the row number where the header #CHROM Starts.
-  row_id <- which(grepl("^#CHROM", vcf$V1)) + 1
-  
-  # splot the single column V1 into multiple columns then later delet unwanted columns
   vcf <- vcf[row_id:.N, data.table::tstrsplit(V1, "\t", fixed = TRUE)][
     , -c(3, 7, 8, 9) # remove unwanted columns ID, FILTER, INFO, FORMAT
   ]
   
-  # select the columns that contain the genotype info
-  cols <- names(vcf)[6:length(vcf)] # first 5 don't need processing
+  cols <- names(vcf)[6:length(vcf)] # first 6 don't need processing
   
-  # extract the genotype info from th slected columns
   vcf[, (cols) := lapply(.SD, function(g) {
     substring(g, 1, regexpr(":", g, fixed = TRUE) - 1) # 1st position only
   }), .SDcols = cols]
