44dce30337364ea26bb8d8ffa603c3257e5fc9b4
Maranita <93143789+Maranita@users.noreply.github.com>
2024-10-08 17:57:18 +0000

    added function to plot the distrbution of snps in the filtered details dataset. Also added plots to visualise the potentian number of samples and loci that will remain after applying the soft filteration function.

--- R/filter_snps_and_samples.R
+++ R/filter_snps_and_samples.R
@@ -13,8 +13,12 @@
 #'    or not. Iterative filtering consists in soft filtering the input object by
 #'    varying the missingness cut-off from 0.8 to 0.1, and the MAF cut-off from
 #'    0.4 to 0.1. Default is \code{FALSE}.
+#'    @param hard A boolean that specify whether to perform hard iterative filtering
+#'    or not. The hard Iterative filtering is done by filtering the input object by
+#'    varying the missingness cut-off from 0.8 to 0.1, and filters out the inputted MAF and min_qual.
+#'     Default is \code{FALSE}.
 #' @param maf_cutoff the MAF cut-off. loci with a MAF < maf_cutoff will be
-#'    discarded
+#'    discarded. default = 0.01
 #'
 #' @return a filtered SNPdata object
 #' @examples
@@ -112,6 +116,8 @@ filter_snps_samples <- function(snpdata,
     }
     
     snpdata[["index"]] <- snpdata[["index"]] + 1L
+    plot_snpDistribution(snpdata[["details"]])
+    
   }
   
   
@@ -132,24 +138,56 @@ filter_snps_samples <- function(snpdata,
 filter_soft <- function(details, meta) {
   # soft filtering snps and samples by varying their missingness cut-off from
   # 0.8 until 0.1 by a step of 0.1
+  num_sampleLoci =data.frame(matrix(rbind(c(nrow(meta), "samples", 1), c(nrow(details), "loci", 1)), ncol = 3))
+  colnames(num_sampleLoci) = c("length", "variable", "missingness")
   missingness_cutoff <- seq(0.8, 0.1, -0.1)
   message("soft filter on missingness")
+ 
   for (c in missingness_cutoff) {
-    idx_snps <- which(details[["percentage_missing_samples"]] >= c)
-    idx_sample <- which(meta[["percentage_missing_sites"]] <= c)
-    cli::cli_alert_info("cut-off = {c}: remaining {length(idx_snps)} SNPs and \\
-                        {length(idx_sample)} samples.")
+    idx_snps <- length(which(details[["percentage_missing_samples"]] <= c))
+    idx_sample <- length(which(meta[["percentage_missing_sites"]] <= c))
+    num_sampleLoci = rbind.data.frame(num_sampleLoci, c(idx_sample, "samples", c), c(idx_snps, "loci", c))
+    cli::cli_alert_info("cut-off = {c}: remaining {idx_snps} SNPs and \\
+                        {idx_sample} samples.")
+    
   }
+  
+  # Change the colors manually
+  p <- ggplot(data = num_sampleLoci, aes(x = missingness, y = length, fill = variable)) +
+    geom_bar(stat = "identity", color = "black", position = position_dodge()) +
+    theme_minimal() +
+    geom_text(aes(label = length), hjust = -0.5, color = "black",
+              position = position_dodge(0.9), size = 3) +
+    scale_fill_manual(values = c('#999999', 'grey25')) +
+    labs(x = "Percentage Missingness", 
+         y = "Number of Variables", 
+         title = "Horizontal Bar Plot of Length by Percentage Missingness") +
+    coord_flip()  # Flip the axes to create a horizontal bar plot
+    #theme_classic()
+  # Use custom colors
+  print(p)
   
   # soft filtering snps by varying their MAF cut-off from
   # 0.4 until 0.1 by a step of 0.1
+  num_sampleLoci1 =data.frame(matrix(rbind(c(nrow(details), 0)), ncol = 2))
+  colnames(num_sampleLoci1) = c("length", "MAF")
   message("\nsoft filter on MAF")
-  maf_cutoff <- seq(0.4, 0.1, -0.1)
+  maf_cutoff <- seq(0.05, 0.01, -0.01)
   for (c in maf_cutoff) {
-    idx_snps <- which(details[["MAF"]] >= c)
-    cli::cli_alert_info("cut-off = {c}: remaining {length(idx_snps)} SNPs.")
+    idx_snps <- length(which(details[["MAF"]] >= c))
+    num_sampleLoci1 = rbind.data.frame(num_sampleLoci1, c(idx_snps, c))
+    
+    cli::cli_alert_info("cut-off = {c}: remaining {idx_snps} SNPs.")
   }
   
+  pp = ggplot(num_sampleLoci1, aes(x=MAF, y=length)) +
+    geom_bar(stat="identity")+theme_minimal()+
+    geom_text(aes(label=length), vjust=-0.5, color="darkblue", size=4, position="stack")+
+    labs(x = "Minor Allele Frequency (MAF)", 
+         y = "number of loci", 
+         title = "Bar Plot of Length by Minor Allele Frequency") #+  # Add title
+    #scale_x_discrete(breaks = num_sampleLoci1$MAF)
+  print(pp)
 }
 
 
@@ -184,5 +222,38 @@ filter_hard <- function(snpdata, min_qual, maf_cutoff) {
   snpdata[["GT"]] = data[(rownames(data) %in% paste0(details[["Chrom"]], ":", details[["Pos"]])),]
   #**filter snps and position from the vcf
   data.table::fwrite(as.data.frame(snpdata[["details"]]), paste0(path, "/details.txt"))
+  plot_snpDistribution(details)
   #apply filteration on list
+}
+
+plot_snpDistribution = function(details){
+  library(ggplot2)
+  details[["Pos"]] = as.numeric(details[["Pos"]])
+  # Define window size (10Kb)
+  window_size <- 1000
+  # Count SNPs per window for each chromosome
+  snp_density <- details %>%
+    mutate(Window = cut(Pos, breaks = seq(0, max(Pos), by = window_size), include.lowest = TRUE)) %>%
+    group_by(Chrom, Window) %>%
+    summarise(SNP_count = n(), .groups = 'drop') %>%
+    mutate(Window_start = as.numeric(gsub("[^0-9.e+-,.]", "", do.call(data.frame, strsplit(as.character(Window), ","))[1,])), 
+           Window_end = Window_start + window_size) #get window start and stop
+  
+  snp_density[["Chrom"]] = as.numeric(substr(snp_density[["Chrom"]], 7, 8))
+  # Create the SNP density plot
+ p= ggplot(snp_density, aes(x = Window_start, y = SNP_count)) +
+    geom_bar(stat = "identity", position = "stack", fill = "steelblue", colour = "darkblue") +  
+    facet_grid(rows = vars(Chrom), scales = "free_y") +  # Correctly facet by rows
+    theme_minimal(base_size = 14) +         # Increase base font size for better readability
+    labs(x = "Genomic Location (bp)", 
+         y = "SNP Density", 
+         title = "SNP Density Plot by Chromosome") +
+    theme(
+      axis.text.x = element_blank(),          # Remove x-axis labels
+      axis.ticks.x = element_blank(),         # Remove x-axis ticks
+      strip.text.y = element_text(size = 12), # Customize facet label size for y-axis
+      axis.text.y = element_blank(),           # Remove y-axis labels
+      axis.ticks.y = element_blank()           # Remove y-axis ticks
+    )
+ print(p)
 }
