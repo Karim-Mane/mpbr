ee6126d3619e55e771693ce4795f8b36a53f7623
Karim-Mane <karimanee@outlook.com>
2026-09-01 19:36:34 +0200

    rename the `drop_samples()` into `remove_samples()`

--- R/drop_samples.R
+++ R/drop_samples.R
@@ -1,45 +1,77 @@
-#' Drop a specific set of samples from the `SNPdata` object
+#' Remove a set of samples from a <SNPdata> object
 #'
-#' @param snpdata The input `SNPdata` object
-#' @param samples_to_be_dropped A vector of samples to be dropped
+#' @param snpdata The input <SNPdata> object
+#' @param samples A vector or a file with one column containing the list of
+#'    samples to be dropped from the provided <SNPdata> object. When the value
+#'    of this argument is a file, the column must be named as `sample`.
 #'
-#' @return A `SNPdata` object where the specified samples have been removed
+#' @return A <SNPdata> object where the specified samples have been removed
 #'
 #' @export
-drop_samples <- function(snpdata, samples_to_be_dropped) {
+#' @examples
+#' \dontrun{
+#'  # create the SNPdata object
+#'  snpdata <- get_snpdata(
+#'    vcf_file = system.file("extdata", "test_data.vcf.gz", package = "mpbr"),
+#'    meta_file = system.file("extdata", "sample_metadata.RDS",
+#'                             package = "mpbr"),
+#'    output_dir = tempdir(),
+#'    gaf = file.path("data", "PlasmoDB-68_Pfalciparum3D7_Curated_GO.gaf.gz"),
+#'    gff = file.path("data", "PlasmoDB-68_Pfalciparum3D7.gff")
+#'  )
+#' } 
+remove_samples <- function(snpdata, samples) {
   checkmate::assert_class(snpdata, "SNPdata", null.ok = FALSE)
-  checkmate::assert_vector(samples_to_be_dropped, any.missing = FALSE,
-                           null.ok = FALSE, min.len = 1L)
-  stopifnot("Some samples in the provided vector are not found in the current
-         data" = all(samples_to_be_dropped %in% snpdata[["meta"]][["sample"]]))
-  idx               <- match(samples_to_be_dropped,
-                             snpdata[["meta"]][["sample"]])
-  tmp_meta          <- snpdata[["meta"]]
-  tmp_meta          <- tmp_meta[-(idx), ]
+  if (!checkmate::check_vector(samples, any.missing = FALSE, null.ok = FALSE,
+                               min.len = 1L)) {
+    checkmate::assert_file_exists(samples, access = "r")
+    target_samples <- data.table::fread(samples, header = TRUE)
+    if (ncol(target_samples) != 1 && names(target_samples) != "sample") {
+      cli::cli_abort(c(
+        i = "Expected a file with one column named as {.val sample}",
+        x = "Incorrect value for the {.strong samples} argument"
+      ))
+    }
+    samples <- target_samples[["sample"]]
+  }
+  
+  # check whether all provided samples are found in the 'meta' table
+  if (!all(samples %in% snpdata[["meta"]][["sample"]])) {
+    cli::cli_abort(c(
+      i = "Some specified samples are not found in the input {.cls SNPdata} \\\
+      object",
+      x = "Invalid sample names were detected."
+    ))
+  }
+  idx <- match(samples, snpdata[["meta"]][["sample"]])
+  tmp_meta <- snpdata[["meta"]]
+  tmp_meta <- tmp_meta[-(idx), ]
   snpdata[["meta"]] <- tmp_meta
-  fields            <- c("GT", "Phased", "Imputed")
+  fields <- c("GT", "Phased", "Imputed")
   for (field in fields) {
     if (field %in% names(snpdata)) {
-      idx              <- match(samples_to_be_dropped,
-                                colnames(snpdata[[field]]))
-      m                <- seq_len(ncol(snpdata[[field]]))
-      m                <- m[-idx]
-      tmp              <- snpdata[[field]]
-      tmp              <- tmp[, m]
+      idx <- match(samples, colnames(snpdata[[field]]))
+      m <- seq_len(ncol(snpdata[[field]]))
+      m <- m[-idx]
+      tmp <- snpdata[[field]]
+      tmp <- tmp[, m]
       snpdata[[field]] <- tmp
     }
   }
   
-  message("Re-calculating the MAF from the 'GT' matrix...")
-  snpdata <- compute_maf(
-    snpdata     = snpdata,
-    genotype    = "GT",
-    include_het = FALSE
-  )
-  message("Re-calculating the percent of missing samples for every SNPs from",
-          "the 'GT' matrix...")
-  snpdata[["details"]][["percentage_missing_samples"]] <-
-    rowSums(is.na(snpdata[["GT"]])) / ncol(snpdata[["GT"]])
+  # re-calculating the MAF from the 'GT' matrix
+  if ("MAF" %in% names(snpdata[["details"]])) {
+    snpdata <- compute_maf(
+      snpdata = snpdata,
+      genotype = "GT",
+      include_het = FALSE
+    )
+  }
+  
+ # re-calculating the percent of missing genotypes for every SNPs from the 'GT'
+ # matrix
+ snpdata[["details"]][["percentage_missing_samples"]] <-
+   rowSums(is.na(snpdata[["GT"]])) / ncol(snpdata[["GT"]])
 
-  snpdata
+  return(snpdata)
 }
