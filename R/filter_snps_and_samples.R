41778210d811f460867aef5b8d9dacc4f98cb5d3
Karim-Mane <karimanee@outlook.com>
2026-09-01 20:51:38 +0200

    update the `filter()` function

--- R/filter_snps_and_samples.R
+++ R/filter_snps_and_samples.R
@@ -1,13 +1,10 @@
-#' Filter a `SNPdata` object
+#' Filter a <SNPdata> object
 #'
 #' SNPs and samples are filtered based on the user-specified thresholds. Both
 #' the `meta`, `details` and the different genotype tables will be affected
 #' depending on the filtration conditions.
 #'
-#' @param snpdata An object of class `SNPdata`
-#' @param min_qual An integer that represents the The minimum call quality
-#'    score. Loci with a quality score below this will be discarded.
-#'    Default is 1000.
+#' @param snpdata An object of class <SNPdata>
 #' @param max_missing_sites A numeric representing the maximum fraction of
 #'    missing sites above which a sample should be dropped. Default is 0.2.
 #' @param max_missing_samples A numeric representing the maximum fraction of
@@ -15,17 +12,32 @@
 #' @param maf_cutoff A numeric representing the minor allele frequency cut-off.
 #'    Loci with a `MAF < maf_cutoff` will be discarded.
 #'
-#' @return a filtered `SNPdata` object
+#' @return a filtered <SNPdata> object
 #' @export
-#'
+#' @examples
+#' \dontrun{
+#'  # create the SNPdata object
+#'  snpdata <- get_snpdata(
+#'    vcf_file = system.file("extdata", "test_data.vcf.gz", package = "mpbr"),
+#'    meta_file = system.file("extdata", "sample_metadata.RDS",
+#'                             package = "mpbr"),
+#'    output_dir = getwd(),
+#'    gaf = file.path("data", "PlasmoDB-68_Pfalciparum3D7_Curated_GO.gaf.gz"),
+#'    gff = file.path("data", "PlasmoDB-68_Pfalciparum3D7.gff")
+#'  )
+#'  # filter the SNPdata object
+#'  snpdata <- filter(
+#'    snpdata = snpdata,
+#'    max_missing_sites = 0.2,
+#'    max_missing_samples = 0.2,
+#'    maf_cutoff = 0.01
+#'  )
+#' }
 filter <- function(snpdata,
-                   min_qual            = 1000L,
-                   max_missing_sites   = 0.2,
+                   max_missing_sites = 0.2,
                    max_missing_samples = 0.2,
-                   maf_cutoff          = 0.01) {
+                   maf_cutoff = 0.01) {
   checkmate::assert_class(snpdata, "SNPdata", null.ok = FALSE)
-  checkmate::assert_numeric(min_qual, lower = 1000L, any.missing = FALSE,
-                            null.ok = FALSE)
   checkmate::assert_numeric(max_missing_sites, lower = 0L, upper = 1L,
                             finite = TRUE, any.missing = FALSE, null.ok = FALSE,
                             len = 1L)
@@ -36,47 +48,68 @@ filter <- function(snpdata,
                             finite = TRUE, any.missing = FALSE, null.ok = FALSE,
                             len = 1L)
   # filter the details table and the genotype matrices
-  x      <- snpdata[["details"]]
+  x <- snpdata[["details"]]
   fields <- names(snpdata)[!(names(snpdata) %in% c("meta", "details", "vcf"))]
-  idx    <- x[["Qual"]] >= min_qual &
-    x[["percentage_missing_samples"]] <= max_missing_samples &
+  # calculate the MAF if it was not
+  if (!"MAF" %in% names(x)) {
+    snpdata <- compute_maf(
+      snpdata = snpdata,
+      include_het = FALSE,
+      genotype = "GT"
+    )
+  }
+  idx <- x[["percentage_missing_samples"]] <= max_missing_samples &
     x[["MAF"]] >= maf_cutoff
-  stopifnot("\nNo locus in VCF file has satisfied the specified QC metrics." =
-              any(idx))
+  if (sum(idx) == 0) {
+    cli::cli_abort(
+      x = "No SNP in the input <SNPdata> object has satisfied the specified \\\
+      filtering thresholds."
+    )
+  }
   if (sum(idx) < nrow(snpdata[["details"]])) {
-    x    <- x[idx, ]
+    x <- x[idx, ]
     snpdata[["details"]] <- x
     for (field in fields) {
       snpdata[[field]] <- snpdata[[field]][idx, ]
     }
   } else if (sum(idx) == nrow(snpdata[["details"]])) {
-    message("All loci have satisfied the specified QC metrics.")
+    cli::cli_alert_success(
+    "All SNPs have satisfied the specified filtering thresholds")
   }
   
   # filter the meta table
   idx <- snpdata[["meta"]][["percentage_missing_sites"]] <= max_missing_sites
-  stopifnot("\nNo sample in VCF file has satisfied the specified QC metrics." =
-              any(idx))
+  if (sum(idx) == 0) {
+    cli::cli_abort(
+      x = "No sample in input <SNPdata> object has satisfied the specified \\\
+      filtering thresholds."
+    )
+  }
+  samples_to_be_dropped <- character()
   if (sum(idx) < nrow(snpdata[["meta"]])) {
     samples_to_be_dropped <- snpdata[["meta"]][["sample"]][!idx]
-    message("\nThe following samples will be removed:\n",
-            paste(samples_to_be_dropped, collapse = ", "))
+    cli::cli_inform(
+      "The following samples will be removed: \\\
+      {.val {toString(samples_to_be_dropped)}}",
+    )
     snpdata[["meta"]] <- snpdata[["meta"]][idx, ]
   } else if (sum(idx) == nrow(snpdata[["meta"]])) {
-    message("\nAll samples have satisfied the specified QC metrics.")
+    cli::cli_alert_success(
+      "All samples have satisfied the specified filtering thresholds."
+    )
   }
   
   # remove bad quality samples from genotype matrices and recalculate both SNPs
   # and sample missingness and MAF
 
   # filter out samples with bad qualities from the genotype tables
-  if (exists("samples_to_be_dropped")) {
+  if (length(samples_to_be_dropped) > 0) {
     m <- match(samples_to_be_dropped, colnames(snpdata[["GT"]]))
     for (field in fields) {
       snpdata[[field]] <- snpdata[[field]][, -m]
     }
 
-    # recalculate the percent of missing data for every SNP
+    # recalculate the percent of missing genotypes for every SNP
     snpdata[["details"]][["percentage_missing_samples"]] <-
       rowSums(is.na(snpdata[["GT"]])) / ncol(snpdata[["GT"]])
     
@@ -84,7 +117,7 @@ filter <- function(snpdata,
     snpdata <- compute_maf(
       snpdata,
       include_het = FALSE,
-      mat_name    = "GT"
+      genotype    = "GT"
     )
   }
   
@@ -92,5 +125,5 @@ filter <- function(snpdata,
   snpdata[["meta"]][["percentage_missing_sites"]] <-
     colSums(is.na(snpdata[["GT"]])) / nrow(snpdata[["GT"]])
   
-  snpdata
+  return(snpdata)
 }
